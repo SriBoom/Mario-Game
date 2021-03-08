d481b7c1674848d998ffef72cf8e705f3ee7f3d5
SriBoom <74517999+SriBoom@users.noreply.github.com>
2021-03-07 18:05:45 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,31 +1,35 @@
-var backgroundImg, bg, clouds;
-var ground, player, marioImage,groundImg, cloudImg;
-var cloudsGroup;
+var backgroundImg, bg, clouds, coins, enemy;
+var ground, player, marioImage,groundImg, cloudImg, coinImg, enemyImg;
+var cloudsGroup, coinGroup, enemyGroup;
 
 function preload(){
  backgroundImg = loadImage("Background Image.jpg");
  marioImage=loadAnimation("mario2.png", "mario1.png");
  groundImg=loadImage("ground.png");
  cloudImg=loadImage("cloud.png");
+ coinImg=loadImage("coin.png");
+ enemyImg=loadAnimation("enemy1.png", "enemy2.png");
 }
 
 function setup() {
- createCanvas(windowWidth,windowHeight);
+ createCanvas(windowWidth/0.5,windowHeight);
 
  //bg=createSprite(windowWidth,windowHeight);
  //bg.addImage(backgroundImg);
  //bg.scale=3;
 
- ground = createSprite(500, 750, windowWidth, 10);
+ ground = createSprite(500, 800, windowWidth, 10);
  ground.addImage(groundImg);
  ground.velocityX = -10;
  ground.x=ground.width/2;
 
  player = createSprite(200, 620, 50, 50);
  player.addAnimation("mario_running",marioImage);
- //player.scale = 0.01;
+ player.scale = 0.5;
 
 cloudsGroup = new Group();
+coinGroup = new Group();
+enemyGroup = new Group();
 }
 
 function draw() {
@@ -35,10 +39,21 @@ function draw() {
     ground.x=ground.width/2;
  }
 
+ if(keyDown("space")){
+   player.velocityY=-10;
+ }
+ player.velocityY=player.velocityY+0.5;
+
+ if(player.isTouching(coinGroup)){
+    coinGroup.destroyEach();
+ }
+
  
- // player.collide(ground);
+ player.collide(ground);
  spawnClouds();
-drawSprites();
+ spawnCoins();
+ spawnEnemy();
+ drawSprites();
 }
 
  function spawnClouds(){
@@ -49,5 +64,25 @@ drawSprites();
      clouds.velocityX=-15;
      clouds.lifetime = 200;
      cloudsGroup.add(clouds);
+    }
+ }
+
+ function spawnCoins(){
+     if(frameCount%50===0){
+        coins=createSprite(random(300, 500), random(500, 600), 100, 100);
+        coins.addImage(coinImg);
+        coins.velocityX=-3;
+        coins.lifetime=250;
+        coinGroup.add(coins);
+     }
+ }
+
+ function spawnEnemy(){
+    if(frameCount%200===0){
+       enemy-createSprite(random(400, 600), 620, 50, 50);
+       enemy.addAnimation("enemy",enemyImg);
+       enemy.velocityX=-6;
+       enemy.lifetime=250;
+       enemyGroup.add(enemy);
     }
  }
